cf55bcdbc88e5787451dd52375fe6395fa973aff
Isaias Pascoal <isaiasxxlee100@gmail.com>
2025-04-01 11:53:06 -0300

    JogoDaVelha

---
+++ JS/JogoDaVelha.js
@@ -0,0 +1,30 @@
+let tabuleiro
+let board
+let aviso
+let jogador
+let linha
+let coluna
+
+function iniciar(){
+    tabuleiro = [1,2,3]
+    board = document.getElementById("board")
+    aviso = document.getElementById("aviso")
+    jogador = 1 
+    
+    console.table(tabuleiro)
+}
+
+function exibir(){
+
+
+}
+
+function jogar(){
+
+
+}
+
+function chegar(){
+
+
+}
